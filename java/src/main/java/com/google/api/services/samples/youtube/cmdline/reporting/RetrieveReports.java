51daf895db7dc85be4f9d2b896ca7b733ebea017
haebin <haebin.na@gmail.com>
2016-07-13 10:38:03 +0900

    Original YouTube API samples package bug fix.

--- java/src/main/java/com/google/api/services/samples/youtube/cmdline/reporting/RetrieveReports.java
+++ java/src/main/java/com/google/api/services/samples/youtube/cmdline/reporting/RetrieveReports.java
@@ -14,8 +14,16 @@
 
 package com.google.api.services.samples.youtube.cmdline.reporting;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.GenericUrl;
 import com.google.api.services.samples.youtube.cmdline.Auth;
 import com.google.api.services.youtubereporting.YouTubeReporting;
 import com.google.api.services.youtubereporting.YouTubeReporting.Media.Download;
@@ -23,18 +31,7 @@ import com.google.api.services.youtubereporting.model.Job;
 import com.google.api.services.youtubereporting.model.ListJobsResponse;
 import com.google.api.services.youtubereporting.model.ListReportsResponse;
 import com.google.api.services.youtubereporting.model.Report;
-
 import com.google.common.collect.Lists;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-
-import javax.print.attribute.standard.Media;
 
 /**
  * This sample retrieves reports created by a specific job by:
